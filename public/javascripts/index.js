3e92dde73d2e241f36fe76138ff6624b94c160e2
davfry <davecfryer@gmail.com>
2015-02-27 12:02:45 -0700

    add files

---
+++ public/javascripts/index.js
@@ -0,0 +1,18 @@
+/**
+ * Module dependencies
+ */
+
+var App = require('poe-ui');
+
+/**
+ * Expose the app
+ */
+
+var app = module.exports = App('hoverfx');
+
+/**
+ * Load plugins here with the 'use' method:
+ *
+ *   app.use(require('modal'));
+ *
+ */
